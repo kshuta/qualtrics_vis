99b2a34b234571b26610a4689da5d186c9444aa4
kshuta <shutakumada@gmail.com>
2022-01-13 20:09:57 -0600

    finshed writing data to local csv file

--- fetch.go
+++ fetch.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"bytes"
 	"encoding/json"
 	"errors"
@@ -22,23 +23,24 @@ func main() {
 	dataCenter := os.Getenv("DATA_CENTER_ID")
 	fileFormat := os.Getenv("FILE_FORMAT")
 
-	fmt.Println(exportSurvey(apiToken, surveyId, dataCenter, fileFormat))
+	if err := exportSurvey(apiToken, surveyId, dataCenter, fileFormat); err != nil {
+		logger.Fatalln(err)
+	}
 }
 
-const baseUrl = "https://%s.qualtrics.com/API/v3/surveys/%s/export-responses/"
+const baseUrl = "https://%s.qualtrics.com/API/v3/surveys/%s/export-responses"
 
 var logger = log.New(os.Stderr, "logger: ", log.LstdFlags|log.Lshortfile)
 
 func exportSurvey(apiToken, surveyId, dataCenter, fileFormat string) error {
-	baseUrl := fmt.Sprintf(baseUrl, dataCenter, surveyId)
-	reqBody := []byte(`{"format": "csv"}`)
-
-	req, err := http.NewRequest(http.MethodPost, baseUrl, bytes.NewBuffer(reqBody))
+	// initial request to create zip file
+	headers := map[string]string{"content-type": "application/json", "x-api-token": apiToken}
+	body := map[string]string{"format": "csv", "useLabels": "true"}
+	url := parseUrl(dataCenter, surveyId)
+	req, err := getInitialRequest(url, http.MethodPost, headers, body)
 	if err != nil {
 		return err
 	}
-	req.Header.Add("content-type", "application/json")
-	req.Header.Add("x-api-token", apiToken)
 
 	client := http.Client{}
 
@@ -47,40 +49,45 @@ func exportSurvey(apiToken, surveyId, dataCenter, fileFormat string) error {
 		return err
 	}
 
+	if res.StatusCode == http.StatusBadRequest {
+		return errors.New("bad request")
+	}
+
 	progResp, err := parseRequestId(res.Body)
 	if err != nil {
 		return err
 	}
-
 	res.Body.Close()
 
 	progressId := progResp.Result.ProgressId
 	progressStatus := progResp.Result.Status
+	logger.Println("ProgressId: ", progressId)
 
 	for progressStatus != "complete" && progressStatus != "failed" {
 		time.Sleep(time.Second * 5)
-		logger.Println("Status: ", progressStatus)
-		requestCheckUrl := baseUrl + progressId
-		req, err = http.NewRequest(http.MethodGet, requestCheckUrl, nil)
-		if err != nil {
-			panic(err)
-			// return err
+		if progressStatus == "" {
+			logger.Printf("Progress status empty, http status: %d\n", res.StatusCode)
+			body, _ := io.ReadAll(res.Body)
+			logger.Printf("%q", string(body))
+			logger.Printf("%q", req.URL)
+			return fmt.Errorf("status response returned %d", res.StatusCode)
 		}
-		req.Header.Add("content-type", "application/json")
-		req.Header.Add("x-api-token", apiToken)
+		logger.Println("Status: ", progressStatus)
+		requestCheckUrl := parseUrl(dataCenter, surveyId, progressId)
+		req, err := getInitialRequest(requestCheckUrl, http.MethodGet, headers, nil)
+		if err != nil {
+			return err
+		}
 
 		res, err = client.Do(req)
 		if err != nil {
-			// return err
-			panic(err)
+			return err
 		}
 
 		progResp, err = parseRequestId(res.Body)
 		if err != nil {
-			// return err
-			panic(err)
+			return err
 		}
-
 		res.Body.Close()
 
 		progressStatus = progResp.Result.Status
@@ -92,20 +99,89 @@ func exportSurvey(apiToken, surveyId, dataCenter, fileFormat string) error {
 
 	fileId := progResp.Result.FileId
 
-	requestDownloadUrl := baseUrl + fileId + "/file"
-	req, err = http.NewRequest(http.MethodGet, requestDownloadUrl, nil)
+	requestDownloadUrl := parseUrl(dataCenter, surveyId, fileId, "/file")
+	req, err = getInitialRequest(requestDownloadUrl, http.MethodGet, headers, nil)
 	if err != nil {
-		// return err
-		panic(err)
+		return err
 	}
 
 	res, err = client.Do(req)
 	if err != nil {
-		// return err
+		return err
+	}
+
+	readContent(res.Body)
+	res.Body.Close()
+
+	return nil
+}
+
+func readContent(body io.ReadCloser) {
+	bodyBytes, err := io.ReadAll(body)
+	if err != nil {
 		panic(err)
 	}
 
-	return nil
+	zipReader, err := zip.NewReader(bytes.NewReader(bodyBytes), int64(len(bodyBytes)))
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Create("data.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	for _, zipFile := range zipReader.File {
+		fmt.Println("Reading file:", zipFile.Name)
+		unzippedFileBytes, err := readZipFile(zipFile)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		file.Write(unzippedFileBytes)
+	}
+}
+
+func getInitialRequest(url, method string, headers map[string]string, body map[string]string) (*http.Request, error) {
+	// url := fmt.Sprintf(baseUrl, dataCenter, surveyId)
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+
+	for key, val := range headers {
+		req.Header.Add(key, val)
+	}
+
+	return req, nil
+}
+
+func parseUrl(dataCenter, surveyId string, paths ...string) string {
+	url := fmt.Sprintf(baseUrl, dataCenter, surveyId)
+	for _, path := range paths {
+		url += "/" + path
+	}
+
+	logger.Println(url)
+
+	return url
+}
+
+func readZipFile(zf *zip.File) ([]byte, error) {
+	f, err := zf.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+	return io.ReadAll(f)
 }
 
 type progressResponse struct {
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/kshuta/qualtrics_vs
 
 go 1.17
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require github.com/joho/godotenv v1.4.0
