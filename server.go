6664c985e9044d9c068a95ae7b91c2913b5b3603
kshuta <shutakumada@gmail.com>
2022-02-08 23:27:13 -0600

    finished monthly data vis

--- server.go
+++ server.go
@@ -262,22 +262,25 @@ var radarChart = `
 				data: {{ $record.CountsString }},
 				backgroundColor: [
 					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
 				],
 				borderColor: [
 					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
 				],
 				borderWidth: 1
-			}]
+			},
+			{{ if not $prevRecord.Empty }}
+			{
+				label: {{ $prevRecord.Month }} + "/" + {{ $prevRecord.Year }},
+				data: {{ $prevRecord.CountsString }},
+				backgroundColor: [
+					'rgba(54, 162, 235, 1)',
+				],
+				borderColor: [
+					'rgba(54, 162, 235, 1)',
+				]
+			}
+			{{ end }}
+			]
 		},
 		options: {
 			responsive: true,
@@ -291,6 +294,13 @@ var radarChart = `
 						weight: 'bold'
 					},
 					textAlign: 'center',
+					formatter: function(value, context) {
+						if (context.dataIndex <= 4) {
+							return value;
+						} else {
+							return "";
+						}
+					},
 				},
 			},
 			responsive: true,
